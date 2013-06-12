a094bbe9a1f3439f893219210caba2dc6be0f847
r-s- <ryanselk@gmail.com>
2013-06-12 13:01:58 -0600

    Added information

--- 102.cpp
+++ 102.cpp
@@ -1,3 +1,14 @@
+﻿//DailyProgrammer Challenge
+//number 127
+//r-s
+
+/*
+Write a function that takes a string s and an integer n, and returns whether or not the string s contains at most n different characters.
+For example, ncset("aacaabbabccc", 4) would return true, because it contains only 3 different characters, 'a', 'b', and 'c', and 3 ≤ 4.
+For how many English words (yes, it's time for this dictionary again!) does ncset(word, 4) hold?
+http://code.google.com/p/dotnetperls-controls/downloads/detail?name=enable1.txt
+*/
+
 #include <iostream>
 #include <set>
 #include <fstream>
